5248188dff6412c24189fc64ab2e90ec5883bf67
Diptajeet Roy <135850587+diptajeet25@users.noreply.github.com>
2023-06-12 07:44:27 +0600

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,c;
+    scanf("%d",&n);
+    for(r=1;r<=n;r++)
+    {
+        for(c=1;c<=r;c++)
+        {
+            printf("%d",c);
+        }
+        printf("\n");
+    }
+}
